dfbf54d594310acb3ce8eef5a3af7c0deed9324d
Michael Ivanov <58866043+m1ke782@users.noreply.github.com>
2023-11-23 23:54:30 +0000

    Update vin.c

--- vin.c
+++ vin.c
@@ -1,13 +1,26 @@
 #include "vin.h"
 #include "tris.h"
 
+int randbetween(int max)
+{
+  static int seed = 0;
+  seed++;
+  srand(time(0) + seed);
+  return rand()%max;
+}
+
+int mod26(int n)
+{
+  return ((n % 26) + 26) % 26;
+}
+
 void vin_encode_str(char* txt, int txtlen, char* key, int keylen)
 {
   for (int i = 0; i < txtlen; i++)
   {
     int vina = i % keylen;
-    int code = (*(key+vina)-'A') + (*(txt+i)-'A');
-    code = code%26;
+    int code = (*(txt+i)-'A') - (*(key+vina)-'A');
+    code = mod26(code);
     *(txt + i) = code + 'A';
   }
 }
@@ -20,6 +33,57 @@ char* vin_encode(char* txt, int txtlen, char* key, int keylen)
   return output;
 }
 
+
+char* vin_bruteforce_key(char* txt, int txtlen, int keylen, float* bestfit)
+{  
+  //generate key
+  char* key = (char*)calloc(keylen+1, sizeof(char));
+  for (int i = 0; i < keylen; i++)
+    *(key+i) = randbetween(26)+'A';
+  float bestFitness = 0;
+
+  for (int i = 0; i < 10000; i++)
+  {
+    //get fitness before
+    char* encodeBefore = vin_encode(txt,txtlen,key,keylen);
+    float fitnessBefore = trigrams_fitness(encodeBefore,txtlen);
+    free(encodeBefore);
+
+    
+    //generate random mutation
+    int toChange = randbetween(keylen);
+    int changeTo = randbetween(26);
+
+    //make random mutation
+    char charBefore = *(key+toChange);
+    *(key+toChange)=changeTo+'A';
+
+
+    //get fitness after
+    char* encodeAfter = vin_encode(txt,txtlen,key,keylen);
+    float fitnessAfter = trigrams_fitness(encodeAfter,txtlen);
+    free(encodeAfter);
+
+
+    if (fitnessAfter <= fitnessBefore)
+    {
+      //bad mutation, revert
+      *(key+toChange) = charBefore;
+    }
+    else
+    {
+      bestFitness = fitnessAfter;
+      printf("%f+\n",fitnessAfter-fitnessBefore);
+    }
+  }
+
+  printf("-----~-----\n");
+  *bestfit = bestFitness;
+  return key;
+}
+
+
+/*
 char* vin_bruteforce_key(char* txt, int txtlen, int keylen, float* bestfit)
 {  
   //generate key
@@ -27,45 +91,40 @@ char* vin_bruteforce_key(char* txt, int txtlen, int keylen, float* bestfit)
   for (int i = 0; i < keylen; i++)
     *(key+i) = 'A';
 
-  //best key
-  char* bestKey = (char*)calloc(keylen+1, sizeof(char));
-  strcpy(bestKey,"????");
-  double bestFitness = 0;
+  strcpy(key,"PSMO");
 
-  int looping = 1;
-  while (looping)
+
+  for (int i = 0; i < 10000; i++)
   {
+    //get fitness before
+    char* encodeBefore = vin_encode(txt,txtlen,key,keylen);
+    float fitnessBefore = trigrams_fitness(encodeBefore,txtlen);
+    //printf("%f) %s\n",fitnessBefore,encodeBefore);
+    free(encodeBefore);
 
-    char* encoded = vin_encode(txt,txtlen,key,keylen);
-    float fit = trigrams_fitness(encoded,txtlen);
+    //make random mutation
+    int toChange = randbetween(keylen);
+    int changeTo = randbetween(26);
+    char keyBefore = *(key+toChange);
+    *(key+toChange) = changeTo + 'A';
 
-    if (fit > bestFitness)
+    //get fitness after
+    char* encodeAfter = vin_encode(txt,txtlen,key,keylen);
+    float fitnessAfter = trigrams_fitness(encodeAfter,txtlen);
+    free(encodeAfter);
+
+    //revert mutation if not beneficial
+    if (fitnessAfter < fitnessBefore)
     {
-      bestFitness = fit;
-      strcpy(bestKey,key);
+      *(key+toChange) = keyBefore;
     }
-  
-    
-    //step up
-    *(key) += 1;
-    for (int i = 0; i < keylen; i++)
-    {
-      if (*(key+i) > 'Z')
-      {
-        //break condition
-        if (i + 1 == keylen)
-        {
-          looping=0;
-          break;
-        }
-        //step up
-        *(key+i) = 'A';
-        *(key+i+1) += 1;
-      }
-    }
+
+    //apply changes
+    if (fitnessBefore > *bestfit) *bestfit = fitnessBefore;
+    if (fitnessAfter > *bestfit) *bestfit = fitnessAfter;
   }
 
-  free(key);
-  *bestfit = bestFitness;
-  return bestKey;
+  
+  return key;
 }
+*/
